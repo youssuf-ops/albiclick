c14cdbd360a58b632090c1ad0f4a0eed1e1a98ea
youssuf-ops <contabilidadeabdula@gmail.com>
2025-11-23 14:08:15 +0000

    Correções de responsividade do navbar e base.html

---
+++ albiclick/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+#from albiclick.forms import FormContacto
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '489caf8c87efe7712d213ec32c355ce0'
+
+if os.getenv('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+database = SQLAlchemy(app)
+
+#para que DB conheça as tabelas antes das rotas
+from albiclick import models
+
+#importaçao correta das rotas
+from albiclick import routes
